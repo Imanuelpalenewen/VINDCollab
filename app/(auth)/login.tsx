4516bd7ff726e2f33ed4335daa3cbb14c9625415
Imanuel P <imanuelpalenewen15.com>
2026-04-17 15:55:12 +0800

    initial project setup

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function TabIndex() {
+export default function LoginScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Tab</Text>
+      <Text>Login Screen</Text>
     </View>
   );
 }
